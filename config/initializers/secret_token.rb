df8750ba3d4ffa459877456d64a165a4b66d2aa3
jermaine <jermainebuckner@gmail.com>
2013-11-12 08:21:24 -0500

    Initial Creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thundercats::Application.config.secret_key_base = '9b7772b884981f8868e6e19092d06906b8b492dec3f3ff5ee348be0693b45cbede5ec99b9a35f2c0d8604be19d28a6cc74c95a6c40bda5c109ba2173ce867b16'
